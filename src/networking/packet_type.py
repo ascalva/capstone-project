93ff9308922199131312d070b6d306f12baa9339
Alberto Serrano-Calva <axs4986@rit.edu>
2022-03-21 18:57:57 -0400

    Added more packet types to enum

---
+++ src/networking/packet_type.py
@@ -0,0 +1,28 @@
+import enum
+
+class PacketType(str, enum.Enum) :
+    
+    ################## IOT-Related Message Types ######################
+
+    # Request made by IOT to get remote broker hostname and subscribe to service.
+    IOT_REQUEST_TO_SUBSCRIBE  = enum.auto()
+
+    # Request made by IOT to get broker hostname and start publishing.
+    IOT_REQUEST_TO_PUBLISH    = enum.auto()
+
+    # Lead node/hub response with broker hostname.
+    IOT_RESPONSE              = enum.auto()
+
+    # Message from IOT device/service notifying that it's done publishing.
+    IOT_END                   = enum.auto()
+
+
+    ############## Database Node-Related Message Types ################
+    DBH_ADVERT                = enum.auto()
+    DBH_NOT_DEAD              = enum.auto()
+
+
+    ########################## Error Messages #########################
+
+    SERVICE_NOT_FOUND         = enum.auto()
+    PARSING_ERROR             = enum.auto()
