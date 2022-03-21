57f3ea5336300ee7a8907e44cd65e409db07053c
Alberto Serrano-Calva <axs4986@rit.edu>
2022-03-21 18:59:05 -0400

    Added function for processing iot requests to publish; stubbed out queue to send responses aside from ads

--- src/hub.py
+++ src/hub.py
@@ -3,10 +3,10 @@ import threading
 import time
 import json
 import os
+import queue
 
-from networking import Graph
+from networking import Graph, PacketType
 from storage    import ServiceTable
-from common     import PacketType
 
 
 class DBNode :
@@ -15,9 +15,10 @@ class DBNode :
         # TODO: Need to routinely check for updates to topology.
         # TODO: Need way of updating topology in the case of node failure.
 
-        self.graph  = Graph(hostname)
         self.host   = hostname
+        self.graph  = Graph(self.host)
         self.table  = ServiceTable(self.graph.getNeighbors())
+        self.queue  = queue.Queue()
         self.sport  = 8000
         self.dport  = 8001
 
@@ -28,13 +29,24 @@ class DBNode :
         self.sock_send.bind(("", self.dport))
 
         
+    def iotProcessReqToAdd(self, data) :
+        service_name = data["topic"]
+        iot_hostname = data["sender"]
+        
+        # Add to current host's services, will be broadcasted to neighbors.
+        self.table.addServiceToHost(self.host, service_name)
+
+        # TODO: Add response to queue.
+        
+
     def processPacket(self, packet) : 
         getProcessor = {
-            PacketType.IOT_REQUEST  : lambda _ : _,
-            PacketType.IOT_RESPONSE : lambda _ : _,
-            PacketType.IOT_END      : lambda _ : _,
-            PacketType.DBH_NOT_DEAD : lambda _ : _,
-            PacketType.DBH_ADVERT   : lambda data : self.table.readPacket(data)
+            PacketType.IOT_REQUEST_TO_PUBLISH   : lambda data : self.iotProcessReqToAdd(data),
+            PacketType.IOT_REQUEST_TO_SUBSCRIBE : lambda _ : _,
+            PacketType.IOT_RESPONSE             : lambda _ : _,
+            PacketType.IOT_END                  : lambda _ : _,
+            PacketType.DBH_NOT_DEAD             : lambda _ : _,
+            PacketType.DBH_ADVERT               : lambda data : self.table.readPacket(data)
         }
 
         data = json.loads(packet.decode("utf-8"))
@@ -86,6 +98,8 @@ class DBNode :
                 message = self.prepareAdPacket(neighbor)
                 self.sock_send.sendto(message, (neighbor, self.sport))
                 self.table.markNeighborUTD(neighbor)
+            
+            # TODO: Check msg queue.
 
 
     def run(self) :
@@ -94,12 +108,12 @@ class DBNode :
 
         ######## TEST CODE ########
         if self.host == "A_" :
-            self.table.addHost(self.host, self.host, ["temperature", "pressure"])
+            self.table._ServiceTable__addHost(self.host, self.host, ["temperature", "pressure"])
             self.runAdverts()
         
         elif self.host == "B_" :
             time.sleep(2)
-            self.table.addHost(self.host, self.host, ["baby_monitor"])
+            self.table._ServiceTable__addHost(self.host, self.host, ["baby_monitor"])
             self.runAdverts()
 
         else :
