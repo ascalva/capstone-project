9280e28e3aff83ed6805195f9f60d50eee34ff8e
Alberto Serrano-Calva <axs4986@rit.edu>
2022-04-06 21:27:12 -0400

    Stubbed out iot service device; is able to connect properly to local ad node

--- src/iot/types/iot_service.py
+++ src/iot/types/iot_service.py
@@ -1,12 +1,27 @@
+import threading
+import socket
+
 from typing     import Dict, Optional
+from common     import PacketType, ServiceType
 from ..iot_base import IOT_Base
 
-class Service(IOT_Base) :
-    def __init__(self, topic: str = "temperature", data_file: Optional[str] = None):
-        super().__init__(topic, data_file)
 
-        # TODO: Create thread for listening.
+class Service(IOT_Base) :
+    def __init__(self, topic: str, data_file: Optional[str] = None):
+        super().__init__(topic)
+
+        # TODO: create socket and thread for receiving additional data
     
 
-    def executeService(self) :
+    def initBrokerConnection(self) :
+        msg = {
+            "type"         : PacketType.IOT_REQUEST, 
+            "service_type" : ServiceType.SERVICE,
+            "sender"       : self.hostname,
+            "topic"        : self.topic
+        }
+        _ = self.identifyBroker(msg)
+
+
+    def start(self) :
         pass
