3fabb90de354192bfbf6b9b95a7cd58e86a797f7
Alberto Serrano-Calva <axs4986@rit.edu>
2022-04-11 23:26:09 -0400

    Added delay env var

--- src/start_iot.py
+++ src/start_iot.py
@@ -1,9 +1,14 @@
 import os
+import time
 from iot import IOT_Factory
 
 if __name__ == "__main__" :
     topic    = os.environ.get("TOPIC", "")
     iot_type = os.environ.get("IOT_TYPE", "sensor")
+    
+    # Wait before creating IOT device.
+    sleep_time = int(os.environ.get("SLEEP", "0"))
+    time.sleep(sleep_time)
 
     # Get IOT device and start.
     IOT_Factory()[iot_type](topic=topic)
