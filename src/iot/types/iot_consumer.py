418f43bb41460e18b0274b23e109822aac29dc7b
Alberto Serrano-Calva <axs4986@rit.edu>
2022-04-28 21:07:29 -0400

    Commented out eval code

--- src/iot/types/iot_consumer.py
+++ src/iot/types/iot_consumer.py
@@ -1,4 +1,5 @@
 import time
+import random
 import json
 
 from socket     import error as SocketException
@@ -9,6 +10,9 @@ from ..iot_base import IOT_Base
 class Consumer(IOT_Base) :
     def __init__(self, topic: str = ""):
         super().__init__(topic)
+
+        # Output performance information.
+        # self.output = open(f"./data/data/consumer_{self.hostname}.txt", 'w')
 
         self.consume_service_type = {
             ServiceType.SENSOR  : self.subscribeToSensor,
@@ -36,10 +40,13 @@ class Consumer(IOT_Base) :
             "consumer_type" : ConsumerAction.JOIN,
             "sender"        : self.hostname
         }
+        
+        # # NOTE: For testing purposes, don't need to try connecting to all devices
+        # #       in network. Speeds up the process when there are many local devices.
+        # known_hub = self.hostname.split("_")[0] + "_"
 
         data = self.identifyBroker(msg, connect=False)
         self.get_host_from_service = self.invertDictOfLists(data["services"])
-        # print(data)
 
 
     def subscribeToSensor(self) :
@@ -80,6 +87,8 @@ class Consumer(IOT_Base) :
         topic_to_sub = "average"
         topic_host   = self.get_host_from_service[topic_to_sub]
 
+        # start_time = time.time()
+
         msg = json.dumps({
             "type"          : PacketType.IOT_REQUEST,
             "service_type"  : ServiceType.CONSUMER,
@@ -95,16 +104,26 @@ class Consumer(IOT_Base) :
         while True :
             packet = self.sock.recv(S.PACKET_SIZE)
             data   = json.loads(packet.decode(S.ENCODING))
-            print(f"Received packet: {data}")
+            # print(f"Received packet: {data}")
+
+            if "status" not in data :
+                print("Status not in data")
+                print(data)
+                continue
 
             if data["status"] == ServiceStatus.BUSY :
                 continue
 
             if ("output" in data) and (data["output"] is not None):
                 output = data["output"]
-                print(f"OUTPUT OF SERVICE {topic_to_sub} : {output}")
+                # print(f"OUTPUT OF SERVICE {topic_to_sub} : {output}")
                 break
+        
+        # total_time = time.time() - start_time
+        # self.output.write(f"{total_time:.3f}\n")
 
 
     def start(self, s_type) :
-        self.consume_service_type[s_type]()
+        
+        while True :
+            self.consume_service_type[s_type]()
